8929ffd5272dc2a5ee9264c96575780df1307c66
github-hash <54845066+github-hash@users.noreply.github.com>
2019-09-05 12:20:01 +0530

    Add files via upload

---
+++ BDCRunner.java
@@ -0,0 +1,13 @@
+package mybdcrunner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="C:\\Users\\gousiya.biradar\\Desktop\\Gousi\\MyDemo_01_Cucumber\\Features99\\Parametrization.feature", glue = "bdcstepdefinations")
+
+public class BDCRunner {
+
+}
